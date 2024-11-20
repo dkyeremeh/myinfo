4e8b46f5e8054f03ee81430788cc2a4ed07a10df
Desmond Kyeremeh <elDekyfin@gmail.com>
2024-11-20 11:55:59 +0000

    k8s changes

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 dev: 
 	(cd services/account; yarn dev) & (cd services/web; yarn dev)
 
-start: 
-	kubectl apply -f k8s/mysql.yml -f k8s/account.yml -f k8s/web.yml
+run: 
+	kubectl apply -f k8s
 
 build:
 	./scripts/build
-	
+
 push:
 	./scripts/push
--- services/account/src/app.ts
+++ services/account/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 5001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
