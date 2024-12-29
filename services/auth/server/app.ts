13571a7cc900a2e12d0acebfa3f67d35187e29c3
Desmond Kyeremeh <elDekyfin@gmail.com>
2024-12-29 21:26:49 +0000

    Restructuring auth

--- services/auth/server/app.ts
+++ services/auth/server/app.ts
@@ -2,14 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes';
 import cors from 'cors';
-import { resolve } from 'path';
+import { join, resolve } from 'path';
 
 dotenv.config({ path: resolve('../../.env') });
+const staticFilesPath = resolve('build.ui');
 
 const app = express();
 
+app.get('*', (req, res) => {
+  res.sendFile(join(staticFilesPath, 'index.html'));
+});
+
 app.use(express.json());
-app.use('/auth', cors(), authRoutes);
+app.use('/api', cors(), authRoutes);
 
 const PORT = process.env.PORT ?? 5051;
 
