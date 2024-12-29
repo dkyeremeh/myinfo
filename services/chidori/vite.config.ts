42c4027b307197c3699677440072fb2c02276cf2
Desmond Kyeremeh <elDekyfin@gmail.com>
2024-12-29 19:53:35 +0000

    renamed chidori/src to chidori/ui

--- services/chidori/vite.config.ts
+++ services/chidori/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: 'ui', // Specify the directory containing the index.html
+  build: {
+    outDir: '../dist', // Output directory relative to the root
+  },
+});
